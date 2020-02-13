632914a333afddaa65af3ef68843302bb1942a04
TatsuhiroAbe <tatsuhiro.no.jones@gmail.com>
2020-02-13 11:02:28 +0900

    add some questions

--- main.cpp
+++ main.cpp
@@ -13,67 +13,6 @@
 
 using namespace std;
 
-class RankNode {
-public:
-    int left_size = 0;
-    RankNode *left;
-    RankNode *right;
-    int data = 0;
-
-    RankNode (int d) {
-        data = d;
-    }
-
-    void insert(int d) {
-        if (d <= data) {
-            if (left != nullptr) {
-                left->insert(d);
-            } else {
-                left = new RankNode(d);
-            }
-            left_size++;
-        } else {
-            if (right != nullptr) {
-                right->insert(d);
-            } else {
-                right = new RankNode(d);
-            }
-        }
-    }
-
-    int getRank(int d) {
-        if (d == data) {
-            return left_size;
-        } else if (d < data) {
-            if (left == nullptr) {
-                return -1;
-            } else {
-                return left->getRank(d);
-            }
-        } else {
-            int right_rank = (right == nullptr) ? -1 : right->getRank(d);
-            if (right_rank == -1) {
-                return -1;
-            } else {
-                return left_size + 1 + right_rank;
-            }
-        }
-    }
-};
-
-RankNode *root = nullptr;
-
-void track(int num) {
-    if (root == nullptr) {
-        root = new RankNode(num);
-    } else {
-        root->getRank(num);
-    }
-}
-
-int getRank(int num) {
-    return root->getRank(num);
-}
 
 int main() {
     
